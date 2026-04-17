43cabe3b377ba1c8fe87487de27d9c8bbed2fdbf
dimpennn <dmitrijpenkivskij@gmail.com>
2026-04-17 08:24:40 +0300

    feat: implement node initialization function for linked list

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -3,9 +3,19 @@
 #include <stdbool.h>
 
 typedef struct Node {
-    int data;
-    struct Node* next;
+    int value;
+    struct Node *next;
 } Node;
+
+Node * node_init(int item) {
+    Node *node_p;
+    node_p = malloc(sizeof(struct Node));
+    *node_p = (Node){
+        .value = item,
+        .next = NULL
+    };
+    return node_p;
+}
 
 int main() {
 
