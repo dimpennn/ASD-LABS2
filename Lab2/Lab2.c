e10047fa5f64c901c96013da6da7a95fe6d86a7c
dimpennn <dmitrijpenkivskij@gmail.com>
2026-04-19 14:18:28 +0300

    feat: initialize creation of 1st node refactor: move gitignode 1 lever higher

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -4,19 +4,20 @@
 
 typedef struct Node {
     int value;
-    struct Node *next;
+    struct Node *next_p;
 } Node;
 
-Node * node_init(int item) {
+Node * create_node(int item) {
     Node *node_p;
     node_p = malloc(sizeof(struct Node));
     *node_p = (Node){
         .value = item,
-        .next = NULL
+        .next_p = NULL
     };
+    printf("ptr=%d info=%d next=%d\n",node_p, node_p->value,node_p->next_p);
     return node_p;
 }
 
 int main() {
-
+    Node *head_p = create_node(10);
 }
